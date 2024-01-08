42df0abdc88fbaf8047deb7db0bf9f7c72a5c3e5
Leonardo FRONT-END <97189523+leoDesenvolvedorWeb@users.noreply.github.com>
2024-01-08 10:24:05 -0300

    aula 10

---
+++ Aula 010/index.js
@@ -0,0 +1,12 @@
+class Pessoal{
+    constructor (nome, idade, altura, genero){
+        this.nome = nome,
+        this.idade = idade,
+        this.altura = altura,
+        this.genero = genero
+    }
+
+    nomeIdade = function(){
+        return this.nome + " " + this.idade;
+    }
+}
