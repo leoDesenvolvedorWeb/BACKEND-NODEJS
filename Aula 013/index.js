12352551d47d30f49f5a19f3271d73fce843b286
Leonardo FRONT-END <97189523+leoDesenvolvedorWeb@users.noreply.github.com>
2024-01-12 11:48:50 -0300

    jsom

--- Aula 013/index.js
+++ Aula 013/index.js
@@ -1,1 +1,20 @@
-console.log("Hello world");
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req,res) =>{
+    res.send("hello world");
+})
+
+app.get("/contato", (req,res) =>{
+    res.send("nosso contato:  helloworld@gmail.com");
+})
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
